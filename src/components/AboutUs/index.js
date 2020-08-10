68196abd63105aa7b91ce33dfab605b66534eec3
Kama <kama@wietrzyk.com>
2020-08-10 21:36:24 +0200

    AboutUs section added

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,6 +1,19 @@
 import React from "react";
+import trzyKawy from './trzyKawy.jpg';
+import './styles.scss'
 
 const AboutUs = () => (
-    <div id="about" style={{ width: "100%", height: "30rem", backgroundColor: "lightgrey" }}>testowy box</div>
+    <div className="AboutUs" id="about">
+        <div className="AboutUs-pic">
+            <img src={trzyKawy} alt="#kawa trzy kawy" />
+        </div>
+        <div className="AboutUs-text">
+            <h2 className="hasztag">O NAS</h2>
+            <hr />
+            <h4>Cześć!</h4>
+            <p>Nazywamy się <span className="hasztag">#KAWA</span> i jesteśmy braćmi, którzy wymarzyli sobie wspólny biznes.</p>
+            <p>Jest nas 3 - Kuba, Tomek i Mateusz, a do kompletu jedna ONA - Kaczka, która dostała od nas drugie życie i po wielu miesiącach ciężkiej pracy stała się przepięknym, jedynym w swoim rodzaju #coffeetruckiem.</p>
+        </div>
+    </div>
 )
 export default AboutUs
