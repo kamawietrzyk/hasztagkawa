4fb08af2dae70554777890a7e950cfac89938d5a
Kama <kama@wietrzyk.com>
2020-08-26 12:33:04 +0200

    Text corrections

--- src/components/Eco/index.js
+++ src/components/Eco/index.js
@@ -13,14 +13,14 @@ const Eco = () => (
                     <span className="icon">
                         <FontAwesomeIcon icon={faGlobeEurope} size="3x" />
                     </span>
-                    <p>Kwestia troski o środowisko jest dla nas niezwykle istotna, dlatego używane przez nas kubki wykonane są z tektury i bio tworzywa PLA (powstałego na bazie skrobi kukurydzianej - kompostuje się w około 80 dni). Nasze nakładki na wynos są tekturowe, serwetki to niebielony papier z recyklingu, a słomki wykonane są z tektury i powleczone wewnątrz woskiem pszczelim.</p>
+                    <p>Kwestia troski o środowisko jest dla nas niezwykle istotna - nie rzucamy sloganami na lewo i prawo, tylko działamy.  Nurt #zerowaste od początku był naszym drogowskazem, także gdy odnawialiśmy busa, dlatego do remontu i zabudowy używaliśmy części z recyklingu oraz drewna, a całość drewnianego wnętrza busa zabezpieczyliśmy naturalnymi woskami i olejami.</p>
                 </div>
                 <div className="box-two">
                     <span className="icon">
                         <FontAwesomeIcon icon={faChild} size="2x" />
                         <FontAwesomeIcon icon={faChild} size="3x" />
                     </span>
-                    <p>Każdy z naszej trójki ma dzieci – nie chcemy pozostawić ich w świecie, w którym przez ludzki egoizm utoną w morzu plastikowych śmieci. Nurt #zerowaste od początku był naszym drogowskazem, także gdy odnawialiśmy busa, dlatego do remontu i zabudowy używaliśmy części z recyklingu oraz drewna, a całość drewnianej zabudowy zabezpieczyliśmy naturalnymi woskami i olejami.</p>
+                    <p>Każdy z naszej trójki ma dzieci – nie chcemy pozostawić ich w świecie, w którym przez ludzki egoizm utoną w morzu plastikowych śmieci. Używane przez nas kubki wykonane są z tektury i bio tworzywa PLA (powstałego na bazie skrobi kukurydzianej). Nakładki na wynos są tekturowe, serwetki to niebielony papier z recyklingu, a słomki wykonane są z tektury i powleczone wewnątrz woskiem pszczelim.</p>
                 </div>
                 <div className="box-three">
                     <span className="icon">
