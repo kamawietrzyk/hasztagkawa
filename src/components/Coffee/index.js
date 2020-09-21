094fd72d945fa4d25a74568aa126981990cfdc44
Kama <kama@wietrzyk.com>
2020-09-21 14:12:05 +0200

    Pic fix + text update

--- src/components/Coffee/index.js
+++ src/components/Coffee/index.js
@@ -8,8 +8,8 @@ const Coffee = () => (
                 <h2 className="hasztag">KAWA SPECIALTY</h2>
                 <hr />
                 <p>Jakość to nasze drugie imię i w tej kwestii nie uznajemy żadnych kompromisów. Dlatego w naszej Kaczce serwujemy kawę wyłącznie w jakości specialty - kawę, która nie może się równać z żadną inną (wbrew temu, co twierdzą specjaliści od reklamy w TV mówiąc, że najlepsza to jest rozpuszczalna ;) ).</p>
-                <p>Sformułowanie "speciality coffee" pierwszy raz zostało użyte przez Ernę Knutsen w 1974 roku w <i>Tea and Coffee Trade Journal</i>. Odnosiło się ono wówczas do najlepszych ziaren kawy, produkowanych w specjalnych warunkach mikroklimatycznych, pozbawionych defektów i charakteryzujących się możliwie najwyższą jakością. Obecnie słowem "speciality" lub "specialty" określa się takie ziarna kawy, które zostaną ocenione na 80 lub więcej punktów (w stupunktowej skali) przez sędziów sensorycznych, tj. kiperów i Q-graderów. Walory smakowe i zapachowe kaw specialty są nie do porównania z żadnymi innymi kawami z marketowych półek, dlatego gdy raz się ich posmakuje bardzo ciężko jest wrócić do tych poprzednich.</p>
-                <p>Ale według nas życie jest za krótkie na to, żeby pić złą kawę!</p>
+                <p>Sformułowanie "speciality coffee" pierwszy raz zostało użyte przez Ernę Knutsen w 1974 roku w <i>Tea and Coffee Trade Journal</i>. Odnosiło się ono wówczas do najlepszych ziaren kawy, produkowanych w specjalnych warunkach mikroklimatycznych, pozbawionych defektów i charakteryzujących się możliwie najwyższą jakością. Obecnie słowem "speciality" lub "specialty" określa się takie ziarna kawy, które zostaną ocenione na 80 lub więcej punktów (w stupunktowej skali) przez sędziów sensorycznych, tj. kiperów i Q-graderów. Walory smakowe i zapachowe kaw specialty są nie do porównania z żadnymi innymi kawami z marketowych półek.</p>
+                <p>A według nas życie jest za krótkie na to, żeby pić złą kawę!</p>
             </div>
         </div>
     </div>
